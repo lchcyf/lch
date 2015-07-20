992a696dc7ea0c7a4c036b923e29f6690a5a00c1
崔云飞 <cuiyunfei@cuiyunfeideMacBook-Pro.local>
2015-07-20 14:36:54 +0800

    Initial Commit

---
+++ MtGit/MtGit/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MtGit
+//
+//  Created by 崔云飞 on 15/7/20.
+//  Copyright (c) 2015年 LCH. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MtGit/MtGit/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MtGit
+//
+//  Created by 崔云飞 on 15/7/20.
+//  Copyright (c) 2015年 LCH. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
