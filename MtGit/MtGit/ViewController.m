844d6e6b45a01d0090cff3e67a0c34001f483dd2
崔云飞 <cuiyunfei@cuiyunfeideMacBook-Pro.local>
2015-07-20 15:15:35 +0800

    dddddd

--- MtGit/MtGit/ViewController.m
+++ MtGit/MtGit/ViewController.m
@@ -16,7 +16,7 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    self.view.backgroundColor = [UIColor blackColor];
+    self.view.backgroundColor = [UIColor redColor];
     // Do any additional setup after loading the view, typically from a nib.
 }
 
